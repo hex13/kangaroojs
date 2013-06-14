635713d1350dcac6eb84cd722d0af21df70e7d05
litwa <taki.litwa@gmail.com>
2013-06-14 09:49:46 +0200

    removing old code, move SimplePhysics to separate file

---
+++ simplephysics.js
@@ -0,0 +1,40 @@
+function SimplePhysics() {
+    this.update = function() {        
+        this.send('move');
+        this.send('update');
+        this._checkCollisions();    
+        this.send('update');        
+    }    
+    
+    this._checkCollisions = function () {
+        var objects = this.objects;
+        for (var i = 0; i < objects.length - 1; i++) {
+            var a = objects[i].model.obj;
+            var am = a.model;
+            for (var j = i+1; j < objects.length; j++) {
+                var b = objects[j].model.obj;
+                var bm = b.model;
+                if (am().dead || bm().dead) continue;                
+                var ax = am().x + am().vx;
+                var bx = bm().x + bm().vx;
+                var ay = am().y + am().vy;
+                var by = bm().y + bm().vy;                    
+                
+                var dx = ax - bx;
+                var dy = ay - by;
+                
+                if (dx * dx + dy * dy < 50*50) {
+                    am(1).vx /= 55;
+                    am(1).vy /= 55;                
+                    bm(1).vx /= 55;
+                    bm(1).vy /= 55;                
+                    
+                    a.send({name:'collision', collider:b}, kng.send);
+                    b.send({name:'collision', collider:a}, kng.send);
+                }
+
+            }           
+        }
+    };        
+}
+SimplePhysics.prototype = new Container();
