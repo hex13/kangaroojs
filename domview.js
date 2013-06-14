6a1627a8d47bd76a07aecd1d8ebec4e2806d17f9
litwa <taki.litwa@gmail.com>
2013-06-14 08:24:47 +0200

    refactoring Container, adding support for onEvent methods

--- domview.js
+++ domview.js
@@ -1,17 +1,16 @@
 function DOMView() {
-    this.render = function() {
-        this.objects.forEach(function(data) {
-            var style = data.style;            
-            model = data.model();
-            style.left = ~~(model.x) + 'px';
-            style.top =  ~~(model.y + 10) + 'px';  
-            if (model.dead) {
-                style.background = "rgba(" + _.random(50,100) +",0,0,0.7)";
-                style.zIndex = '-10';
-            }
-            if(typeof model.opacity!='undefined')
-                style.opacity = model.opacity;        
-        });
+    var self = this;
+    this.onRender = function(data) {
+        var style = data.style;            
+        model = data.model();
+        style.left = ~~(model.x) + 'px';
+        style.top =  ~~(model.y + 10) + 'px';  
+        if (model.dead) {
+            style.background = "rgba(" + _.random(50,100) +",0,0,0.7)";
+            style.zIndex = '-10';
+        }
+        if(typeof model.opacity!='undefined')
+            style.opacity = model.opacity;        
     }
     this.onAdd = function(data) {
         var el = data.el = document.createElement('div');             
@@ -21,6 +20,16 @@ function DOMView() {
         document.body.appendChild(el);
         el.kngModel = data.model;
     };
-    
+    console.log(this.onRender);
+    console.log(this.render);
+    console.log('XX');            
 };
-DOMView.prototype = new Container;
+DOMView.prototype = new Container(['render']);
+
+function DOMViewTest() {
+    /*var view = new DOMView;
+    alert(view.onRender);
+    alert(view.render);      */
+}
+
+new DOMViewTest;
